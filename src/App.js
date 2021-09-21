e01c150e7882860eef94525773b3f28260f92a13
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-09-21 17:11:04 -0300

    preparando ambiente

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router';
+import Pages from './pages';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route exact component={ Pages.Login } path="/" />
+      <Route exact component={ Pages.Foods } path="/comidas" />
+      <Route exact component={ Pages.Drinks } path="/bebidas" />
+      <Route exact component={ Pages.Details } path="/comidas/:id" />
+      <Route exact component={ Pages.Details } path="/bebidas/:id" />
+      <Route exact component={ Pages.Progress } path="/comidas/:id/in-progress" />
+      <Route exact component={ Pages.Progress } path="/bebidas/:id/in-progress" />
+      <Route exact component={ Pages.Explore } path="/explorar" />
+      <Route
+        exact
+        component={ Pages.ExploreIngredients }
+        path="/explorar/comidas/ingredientes"
+      />
+      <Route
+        exact
+        component={ Pages.ExploreIngredients }
+        path="/explorar/bebidas/ingredientes"
+      />
+      <Route exact component={ Pages.ExploreAreas } path="/explorar/comidas/area" />
+      <Route exact component={ Pages.Profile } path="/perfil" />
+      <Route exact component={ Pages.Made } path="/receitas-feitas" />
+      <Route exact component={ Pages.Favorites } path="/receitas-favoritas" />
+    </Switch>
   );
 }
 
---
+++ src/context/drinksContext.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const drinksContext = createContext();
+
+export default drinksContext;
