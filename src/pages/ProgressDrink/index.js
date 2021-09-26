fb5eacb941eb500e4d732f4670ea22bd4808b107
Demontier <demontier@outlook.com>
2021-09-26 13:58:40 -0300

    requisitos 47, 48 e 49

--- src/pages/ProgressDrink/index.js
+++ src/pages/ProgressDrink/index.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function ProgressDrink() {
+export default function ProgressDrink({ match: { params: { id } } }) {
+  const [drink, setDrink] = useState({});
+
+  useEffect(() => {
+    async function getDrink() {
+      const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const data = await response.json();
+      setDrink(...data.drinks);
+      console.log(data);
+    }
+    getDrink();
+  }, [id]);
+
+  const ingredients = [];
+  const quantity = [];
+  const max = 20;
+  for (let index = 1; index <= max; index += 1) {
+    const key = `strIngredient${index}`;
+    const measureKey = `strMeasure${index}`;
+    if (drink[key]) {
+      ingredients.push(drink[key]);
+    }
+    if (drink[measureKey]) {
+      quantity.push(drink[measureKey]);
+    }
+  }
+
+  function handleChange({ target }) {
+    console.log(target.value);
+  }
+
   return (
     <div>
-      <h1>Receita em progresso</h1>
+      <img
+        data-testid="recipe-photo"
+        style={ { height: '150px' } }
+        src={ drink.strDrinkThumb }
+        alt=""
+      />
+      <h1 data-testid="recipe-title">{ drink.strDrink }</h1>
+      <button data-testid="share-btn" type="button">Compartilhar</button>
+      <button data-testid="favorite-btn" type="button">Favoritar</button>
+      <p data-testid="recipe-category">{ drink.strAlcoholic }</p>
+      {ingredients.map((ingredient, index) => (
+        <p data-testid={ `${index}-ingredient-step` } key={ index }>
+          {`${ingredient} ${quantity[index]}`}
+          <input
+            onChange={ handleChange }
+            value={ ingredient }
+            id={ ingredient }
+            type="checkbox"
+          />
+        </p>
+      ))}
+      <p data-testid="instructions">{ drink.strInstructions }</p>
+      <button data-testid="finish-recipe-btn" type="button">Favoritar</button>
     </div>
   );
 }
-
-export default ProgressDrink;
--- src/pages/ProgressFood/index.js
+++ src/pages/ProgressFood/index.js
@@ -1,11 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function ProgressFood() {
+export default function ProgressFood({ match: { params: { id } } }) {
+  const [meal, setMeal] = useState({});
+
+  useEffect(() => {
+    async function getDrink() {
+      const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const data = await response.json();
+      setMeal(...data.meals);
+    }
+    getDrink();
+  }, [id]);
+
+  const ingredients = [];
+  const quantity = [];
+  const max = 20;
+  for (let index = 1; index <= max; index += 1) {
+    const key = `strIngredient${index}`;
+    const measureKey = `strMeasure${index}`;
+    if (meal[key]) {
+      ingredients.push(meal[key]);
+    }
+    if (meal[measureKey]) {
+      quantity.push(meal[measureKey]);
+    }
+  }
+
+  function handleChange({ target }) {
+    console.log(target.value);
+  }
+
   return (
     <div>
-      <h1>Receita em progresso</h1>
+      <img
+        data-testid="recipe-photo"
+        style={ { height: '150px' } }
+        src={ meal.strMealThumb }
+        alt=""
+      />
+      <h1 data-testid="recipe-title">{ meal.strMeal }</h1>
+      <button data-testid="share-btn" type="button">Compartilhar</button>
+      <button data-testid="favorite-btn" type="button">Favoritar</button>
+      <p data-testid="recipe-category">{ meal.strCategory }</p>
+      {ingredients.map((ingredient, index) => (
+        <p data-testid={ `${index}-ingredient-step` } key={ index }>
+          {`${ingredient} ${quantity[index]}`}
+          <input
+            onChange={ handleChange }
+            value={ ingredient }
+            id={ ingredient }
+            type="checkbox"
+          />
+        </p>
+      ))}
+      <p data-testid="instructions">{ meal.strInstructions }</p>
+      <button data-testid="finish-recipe-btn" type="button">Favoritar</button>
     </div>
   );
 }
-
-export default ProgressFood;
