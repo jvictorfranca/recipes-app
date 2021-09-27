551f527cbbf2c3aeace514059a4e0858b85662ea
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-09-27 18:58:10 -0300

    requisito 67 ao 74

--- src/pages/Explore/index.js
+++ src/pages/Explore/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 
 function Explore() {
+  const history = useHistory();
   return (
     <div className="explore">
       <Header title="Explorar" />
+      <button
+        type="button"
+        data-testid="explore-food"
+        onClick={ () => history.push('/explorar/comidas') }
+        style={ { marginTop: '50px' } }
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('/explorar/bebidas') }
+        style={ { marginTop: '50px' } }
+      >
+        Explorar Bebidas
+      </button>
       <Footer />
     </div>
   );
--- src/pages/ExploreDrinks/index.js
+++ src/pages/ExploreDrinks/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 
 function ExploreDrinks() {
+  const history = useHistory();
+  async function aleatory() {
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+    const data = await response.json();
+    const id = data.drinks[0].idDrink;
+    history.push(`/bebidas/${id}`);
+  }
   return (
     <div className="explore-drinks">
       <Header title="Explorar Bebidas" />
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+        style={ { marginTop: '50px' } }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ aleatory }
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
