70b4cb3137d9bc4648a38cfff5a39c84ceac7d83
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-09-27 16:53:11 -0300

    resolve ate o ultimo requisito

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,15 +1,58 @@
 import React from 'react';
+import propTypes from 'prop-types';
 
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 
-function Profile() {
+import './styles.css';
+
+function Profile({ history }) {
+  const { email } = JSON.parse(localStorage.user);
   return (
-    <div className="profile">
+    <main className="profile">
       <Header title="Perfil" />
+      <p data-testid="profile-email" className="profile-email">{email}</p>
+      <div className="profile-buttons-container">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => {
+            history.push('/receitas-feitas');
+          } }
+        >
+          Receitas Feitas
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => {
+            history.push('/receitas-favoritas');
+          } }
+        >
+          Receitas Favoritas
+
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => {
+            localStorage.clear();
+            history.push('/');
+          } }
+        >
+          Sair
+        </button>
+      </div>
       <Footer />
-    </div>
+    </main>
   );
 }
+
+Profile.propTypes = {
+  history: propTypes.shape({
+    push: propTypes.func,
+  }).isRequired,
+};
 
 export default Profile;
