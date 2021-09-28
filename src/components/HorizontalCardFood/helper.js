917525195404807703ed2120e655108746be49c4
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-09-28 19:49:50 -0300

    resolve linter

--- src/components/HorizontalCardFood/helper.js
+++ src/components/HorizontalCardFood/helper.js
@@ -1,17 +1,6 @@
 function RemoveFavorite(recipe, type, isFavorite) {
   let favoritas = JSON.parse(localStorage.getItem('favoriteRecipes'));
-  let indexFinal = null;
   console.log(recipe, type, recipe[type], favoritas);
-
-  if (favoritas) {
-    for (let indexFood = 0; indexFood < favoritas.length; indexFood += 1) {
-      if (favoritas[indexFood].id === recipe[type]) {
-        indexFinal = indexFood;
-      }
-    }
-  } else {
-    favoritas = [];
-  }
 
   if (isFavorite) {
     const newFavoritas = favoritas.filter((a) => a.id !== recipe.id);
