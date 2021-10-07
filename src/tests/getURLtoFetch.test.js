63eb82ea41d01253e59549553c05e3a8389833cb
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-07 02:26:18 -0300

    faz o teste das urls com 100% de coverage do getURLtoFetch

--- src/tests/getURLtoFetch.test.js
+++ src/tests/getURLtoFetch.test.js
@@ -7,26 +7,23 @@ describe(('testa a função getURLtoFetch'), () => {
     expect(URL).toBe(undefined);
     expect(alertMock).toBeCalledTimes(1);
   });
-
   test(('testa caso de mais de uma letra bebida'), () => {
     const URL = getURLtoFetch('/bebidas', 'radioFirstLetter', 'aa');
     expect(URL).toBe(undefined);
     expect(alertMock).toBeCalledTimes(2);
   });
-
   test(('testa caso comidas e nome'), () => {
     const URL = getURLtoFetch('/comidas', 'radioName', 'pie');
     expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/search.php?s=pie');
   });
   test(('testa caso comidas e ingrediente'), () => {
-    const URL = getURLtoFetch('/comidas', 'radioIngredient', 'apple');
-    expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/filter.php?i=apple');
+    const URL = getURLtoFetch('/comidas', 'radioIngredient', 'a');
+    expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/filter.php?i=a');
   });
   test(('testa caso comidas e primeira letra'), () => {
     const URL = getURLtoFetch('/comidas', 'radioFirstLetter', 'a');
     expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
   });
-
   test(('testa caso bebidas e nome'), () => {
     const URL = getURLtoFetch('/bebidas', 'radioName', 'chocolateMilk');
     expect(URL).toEqual('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=chocolateMilk');
@@ -43,10 +40,11 @@ describe(('testa a função getURLtoFetch'), () => {
     const URL = getURLtoFetch('/bebidas');
     expect(URL).toEqual(undefined);
   });
-  test(('testa caso radioname '), () => {
-    const URL = getURLtoFetch('/comidas', 'radioIngredient');
-    expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/filter.php?i=undefined');
+  test(('testa caso comidas '), () => {
+    const URL = getURLtoFetch('/comidas');
+    expect(URL).toEqual(undefined);
   });
+
   test(('testa caso inputValue'), () => {
     const URL = getURLtoFetch(undefined, undefined, 'a');
     expect(URL).toEqual(undefined);
