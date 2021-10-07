98963cff4d66d09332fcaa4b13129eb8281400a6
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-07 01:56:55 -0300

    faz o teste das urls

---
+++ src/tests/getURLtoFetch.test.js
@@ -0,0 +1,54 @@
+import getURLtoFetch from '../helpers/getURLtoFetch';
+
+describe(('testa a função getURLtoFetch'), () => {
+  const alertMock = jest.spyOn(window, 'alert').mockImplementation();
+  test(('testa caso de mais de uma letra'), () => {
+    const URL = getURLtoFetch('/comidas', 'radioFirstLetter', 'aa');
+    expect(URL).toBe(undefined);
+    expect(alertMock).toBeCalledTimes(1);
+  });
+
+  test(('testa caso de mais de uma letra bebida'), () => {
+    const URL = getURLtoFetch('/bebidas', 'radioFirstLetter', 'aa');
+    expect(URL).toBe(undefined);
+    expect(alertMock).toBeCalledTimes(2);
+  });
+
+  test(('testa caso comidas e nome'), () => {
+    const URL = getURLtoFetch('/comidas', 'radioName', 'pie');
+    expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/search.php?s=pie');
+  });
+  test(('testa caso comidas e ingrediente'), () => {
+    const URL = getURLtoFetch('/comidas', 'radioIngredient', 'apple');
+    expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/filter.php?i=apple');
+  });
+  test(('testa caso comidas e primeira letra'), () => {
+    const URL = getURLtoFetch('/comidas', 'radioFirstLetter', 'a');
+    expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
+  });
+
+  test(('testa caso bebidas e nome'), () => {
+    const URL = getURLtoFetch('/bebidas', 'radioName', 'chocolateMilk');
+    expect(URL).toEqual('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=chocolateMilk');
+  });
+  test(('testa caso bebidas e ingrediente'), () => {
+    const URL = getURLtoFetch('/bebidas', 'radioIngredient', 'milk');
+    expect(URL).toEqual('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=milk');
+  });
+  test(('testa caso bebidas e primeira letra'), () => {
+    const URL = getURLtoFetch('/bebidas', 'radioFirstLetter', 'a');
+    expect(URL).toEqual('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a');
+  });
+  test(('testa caso bebidas '), () => {
+    const URL = getURLtoFetch('/bebidas');
+    expect(URL).toEqual(undefined);
+  });
+  test(('testa caso radioname '), () => {
+    const URL = getURLtoFetch('/comidas', 'radioIngredient');
+    expect(URL).toEqual('https://www.themealdb.com/api/json/v1/1/filter.php?i=undefined');
+  });
+  test(('testa caso inputValue'), () => {
+    const URL = getURLtoFetch(undefined, undefined, 'a');
+    expect(URL).toEqual(undefined);
+  });
+});
