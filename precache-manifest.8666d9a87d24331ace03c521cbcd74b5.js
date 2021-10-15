aa895680bf329f42f8ba44b13152fa96cab88026
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-10-15 02:50:52 -0300

    Updates

---
+++ precache-manifest.8666d9a87d24331ace03c521cbcd74b5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4623da07e201c420df41566534b081",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "24a962f5f1df02db2acb",
+    "url": "/recipes-app/static/css/main.0c5ed690.chunk.css"
+  },
+  {
+    "revision": "12713aa245968c2a65c2",
+    "url": "/recipes-app/static/js/2.e8480299.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/recipes-app/static/js/2.e8480299.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24a962f5f1df02db2acb",
+    "url": "/recipes-app/static/js/main.99e67a3b.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
